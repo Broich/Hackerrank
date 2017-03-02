1b40878a96e16b48bc7f9d25a7d0d6e0745dcd83
Kersten Broich <kersten.broich@gmail.com>
2017-03-02 23:29:56 +0100

    Refactor challege in seperate files

---
+++ Hackerrank/apples_and_oranges.hpp
@@ -0,0 +1,16 @@
+//
+//  apples_and_oranges.hpp
+//  Hackerrank
+//
+//  Created by Kersten Broich on 2017-03-02.
+//  Copyright © 2017 Kersten Broich. All rights reserved.
+//
+
+#ifndef apples_and_oranges_hpp
+#define apples_and_oranges_hpp
+
+#include <stdio.h>
+
+void apples_and_oranges();
+
+#endif /* apples_and_oranges_hpp */
